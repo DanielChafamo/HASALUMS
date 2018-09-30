7d745d20e99ced6cf1277bf301d2d064c89e07f5
DanielC <danielchafamo@college.harvard.edu>
2018-09-30 18:12:22 -0400

    preparing to publish

--- HasaAlums/settings.py
+++ HasaAlums/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -119,7 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 LOGIN_REDIRECT_URL = '/'
-
+STATICFILES_DIRS = (
+	os.path.join(PROJECT_ROOT, 'static')
+)
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
